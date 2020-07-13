06b0ec9becf2d336248d10dc6d968ec8b0339212
Daniil Tolstoukhov <jaz7mutant@ya.ru>
2020-07-13 18:13:40 +0500

    misswritting fixes

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AvailableBooksView, EditBookView, CreateBookView, AllBooksView, UpdateBookStatusView
+from .views import AvailableBooksView, EditBookView, CreateBookView, AllBooksView
 
 
 app_name = "books"
